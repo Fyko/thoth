66af6afca60fdbd570ed72a4a3d0ce8aa73cd847
Carter Himmel <me@fyko.net>
2023-11-09 23:58:27 -0700

    feat: cache the datamuse api

--- src/util/datamuse.ts
+++ src/util/datamuse.ts
@@ -1,4 +1,7 @@
 import { URLSearchParams } from 'node:url';
+import { container } from 'tsyringe';
+import type { RedisManager } from '#structures';
+import { kRedis } from './symbols.js';
 
 export type DatamuseHit = {
 	score: number;
@@ -22,6 +25,8 @@ export enum DatamuseQuery {
 	Triggers = 'rel_trg',
 }
 
+const redis = container.resolve<RedisManager>(kRedis);
+
 export async function fetchDatamuse(query: DatamuseQuery, word: string): Promise<DatamuseResult> {
 	const params = new URLSearchParams();
 	params.append(query, word);
@@ -30,6 +35,12 @@ export async function fetchDatamuse(query: DatamuseQuery, word: string): Promise
 }
 
 export async function fetchDatamuseRaw(query: URLSearchParams): Promise<DatamuseResult> {
+	const qs = query.toString();
+	const key = `datamuse:${qs}`;
+
+	const cached = await redis.client.get(key);
+	if (cached) return JSON.parse(cached);
+
 	const url = `https://api.datamuse.com/words?${query}`;
 
 	const response = await fetch(url, {
@@ -39,5 +50,8 @@ export async function fetchDatamuseRaw(query: URLSearchParams): Promise<Datamuse
 		},
 	});
 
-	return (await response.json()) as DatamuseResult;
+	const data = (await response.json()) as DatamuseResult;
+	await redis.client.set(key, JSON.stringify(data), 'EX', 60 * 60 * 24); // 24 hours
+
+	return data;
 }
