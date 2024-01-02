8f63eb20efa3ba3a9cbbb7aa102a1b08541e6b9d
Carter Himmel <me@fyko.net>
2024-01-02 14:09:36 -0700

    feat: patch font to local version

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -2,10 +2,43 @@ import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 import astroI18next from 'astro-i18next';
+import replace from 'vite-plugin-filter-replace';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+const custom = {
+  name: 'CustomThothIntegration',
+  hooks: {
+    'astro:config:setup': ({ updateConfig }) => {
+      updateConfig({
+        vite: {
+          plugins: [
+            replace.default([
+              {
+                filter: /\.js$/,
+                replace: {
+                  from: /https:\/\/cdn.skyra.pw\/whitney-font\/v2\//g,
+                  to: '/assets/fonts/',
+                },
+              },
+            ]),
+            viteStaticCopy({
+              targets: [
+                {
+                  src: '_redirects',
+                  dest: '',
+                },
+              ],
+            }),
+          ],
+        },
+      });
+    },
+  },
+};
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), astroI18next()],
+  integrations: [react(), astroI18next(), custom],
   prefetch: true,
   output: 'hybrid',
   adapter: cloudflare(),
